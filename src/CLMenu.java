e5d5a90900eff25fd5d7999c5a3bfc20f10a0cc0
James Wienecke <james.a.wienecke@gmail.com>
2022-01-14 12:02:56 -0600

    Continuing to build out the options enum.

--- src/CLMenu.java
+++ src/CLMenu.java
@@ -13,27 +13,7 @@ public class CLMenu {
                 "5. Exit.\n" +
                 "Enter an option (1, 2, 3, 4 or 5):";
 
-        int userResponse = in.getInt(1, 5, mainMenu);
-        Option option = Option.UNKNOWN;
-
-        switch (userResponse) {
-            case 1:
-                option = Option.VIEW;
-                break;
-            case 2:
-                option = Option.ADD;
-                break;
-            case 3:
-                option =  Option.SEARCH;
-                break;
-            case 4:
-                option = Option.DELETE;
-                break;
-            case 5:
-                option = Option.EXIT;
-                break;
-        }
-
-        return option;
+        int response = in.getInt(1, 5, mainMenu);
+        return Option.getOptionFromInt(response);
     }
 }
