a93de34239da7e53a44397dc13a6aedc5540ec28
James Wienecke <james.a.wienecke@gmail.com>
2022-01-14 11:22:32 -0600

    Fixed issue with Input package. Started setting up basics of the CL Menu class.

--- src/CLMenu.java
+++ src/CLMenu.java
@@ -1,3 +1,17 @@
 public class CLMenu {
+    private Input in;
 
+    CLMenu() {
+        in = new Input();
+    }
+
+    void printMenu() {
+        String mainMenu = "1. View contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n" +
+                "Enter an option (1, 2, 3, 4 or 5):";
+        System.out.println(mainMenu);
+    }
 }
