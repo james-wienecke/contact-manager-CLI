42ae4be3758c58c38a54c3ab0f1a6bada5c8e6de
James Wienecke <james.a.wienecke@gmail.com>
2022-01-19 10:21:24 -0600

    Refined phone number handling.

--- src/Contact.java
+++ src/Contact.java
@@ -70,22 +70,38 @@ class Phone {
     private static final int US = 1;
     private static final int INT = 2;
 
-    public Phone(String num) throws Exception {
+    public Phone(String num) throws IllegalArgumentException {
         if (num.length() == 10) {
+            // 111 222 3333
+            // 012 345 6789
             country = "1";
             area = num.substring(0, 2);
             number = num.substring(3);
-        } else if (num.length() >= 11){
+        } else if (num.length() == 11) {
+            // 0 111 222 3333
+            // 0 123 456 789A
+            country = num.substring(0, 0);
+            area = num.substring(1, 3);
+            number = num.substring(4);
+        } else if (num.length() == 12) {
+            // 00 111 222 3333
+            // 01 234 567 89AB
+            country = num.substring(0, 1);
+            area = num.substring(2, 4);
+            number = num.substring(5);
+        } else if (num.length() == 13) {
+            // 000 111 222 3333
+            // 012 345 678 9ABC
             country = num.substring(0, 2);
             area = num.substring(3, 5);
             number = num.substring(6);
         } else {
-            throw new E();
+            throw new IllegalArgumentException();
         }
     }
 
     public String getNumber() {
-        return getNumberFormatted();
+        return getNumberFormatted(-1);
     }
 
     public String getNumberFormatted() {
@@ -103,7 +119,7 @@ class Phone {
                     return String.format("%s (%s) %s-%s", country, area, number.substring(0, 2), number.substring(3));
                 }
             case INT:
-                return String.format("%s-%s-%s-%s", country, area, number.substring(0, 2), number.substring(3));
+                return String.format("+%s %s.%s.%s", country, area, number.substring(0, 2), number.substring(3));
             default:
                 return String.format("%s%s%s", country, area, number);
         }
