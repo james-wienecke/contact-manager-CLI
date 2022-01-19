a687b23156ceb269dcb1c560e5a3606df6a430d6
James Wienecke <james.a.wienecke@gmail.com>
2022-01-19 10:56:31 -0600

    Improved number formatting and added UK-style number display.

--- src/Contact.java
+++ src/Contact.java
@@ -9,7 +9,7 @@ public class Contact {
 
     @Override
     public String toString() {
-        return getName() + " | " + getPhone();
+        return getName() + " | " + getPhoneFormatted(0);
     }
 
     public String getName() {
@@ -18,6 +18,10 @@ public class Contact {
 
     public String getPhone() {
         return phone.getNumber();
+    }
+
+    public String getPhoneFormatted(int option) {
+        return phone.getNumberFormatted(option);
     }
 
     // converts the contact into a String formatted to look like "name,phonenumber"
@@ -69,31 +73,32 @@ class Phone {
     private static final int E164 = 0;
     private static final int US = 1;
     private static final int INT = 2;
+    private static final int UK = 3;
 
     public Phone(String num) throws IllegalArgumentException {
         if (num.length() == 10) {
             // 111 222 3333
             // 012 345 6789
-            country = "1";
-            area = num.substring(0, 2);
+            country = "001";
+            area = num.substring(0, 3);
             number = num.substring(3);
         } else if (num.length() == 11) {
             // 0 111 222 3333
             // 0 123 456 789A
-            country = num.substring(0, 0);
-            area = num.substring(1, 3);
+            country = num.substring(0, 1);
+            area = num.substring(1, 4);
             number = num.substring(4);
         } else if (num.length() == 12) {
             // 00 111 222 3333
             // 01 234 567 89AB
-            country = num.substring(0, 1);
-            area = num.substring(2, 4);
+            country = num.substring(0, 2);
+            area = num.substring(2, 5);
             number = num.substring(5);
         } else if (num.length() == 13) {
             // 000 111 222 3333
             // 012 345 678 9ABC
-            country = num.substring(0, 2);
-            area = num.substring(3, 5);
+            country = num.substring(0, 3);
+            area = num.substring(3, 6);
             number = num.substring(6);
         } else {
             throw new IllegalArgumentException();
@@ -108,19 +113,31 @@ class Phone {
         return getNumberFormatted(0);
     }
 
+    /**
+     *
+     * @param format 0: E164 format 1: US format 2: ambiguously european format, 3: UK-ish format -1: no format
+     * @return
+     */
     public String getNumberFormatted(int format) {
         switch (format) {
             case E164:
-                return String.format("+%s%s%s", country, area, number);
+                return String.format("+%s%s%s", country.substring(1), area, number);
             case US:
-                if (country.equalsIgnoreCase("1")) {
-                    return String.format("(%s) %s-%s", area, number.substring(0, 2), number.substring(3));
+                if (country.equalsIgnoreCase("001")) {
+                    // (111)222-3333
+                    return String.format("(%s) %s-%s", area, number.substring(0, 3), number.substring(3));
                 } else {
-                    return String.format("%s (%s) %s-%s", country, area, number.substring(0, 2), number.substring(3));
+                    // +00 (111)222-3333
+                    return String.format("+%s (%s)%s-%s", country.substring(1), area, number.substring(0, 3), number.substring(3));
                 }
             case INT:
-                return String.format("+%s %s.%s.%s", country, area, number.substring(0, 2), number.substring(3));
+                // +00 111.222.3333
+                return String.format("+%s %s.%s.%s", country.substring(1), area, number.substring(0, 3), number.substring(3));
+            case UK:
+                // 00 11 1222 3333
+                return String.format("%s 0%s %s%s %s", country.substring(1), area.substring(0, 2), area.substring(2), number.substring(0, 3), number.substring(3));
             default:
+                // 0001112223333
                 return String.format("%s%s%s", country, area, number);
         }
     }
