30273e7b5ef79a88cdc70e489711280dfbe5ffab
James Wienecke <james.a.wienecke@gmail.com>
2022-01-19 08:23:17 -0600

    Built out a Name inner class to better encapsulate the Contact data.

--- src/Contact.java
+++ src/Contact.java
@@ -1,19 +1,19 @@
 public class Contact {
-    private String name;
+    private Name name;
     private long phone;
 
     Contact(String name, long phone) {
-        this.name = name;
+        this.name = new Name(name);
         this.phone = phone;
     }
 
     @Override
     public String toString() {
-        return name + " | " + phone;
+        return getName() + " | " + getPhone();
     }
 
     public String getName() {
-        return name;
+        return name.getFullName();
     }
 
     public long getPhone() {
@@ -22,6 +22,41 @@ public class Contact {
 
     // converts the contact into a String formatted to look like "name,phonenumber"
     public String formatForStorage() {
-        return String.format("%s,%s", name, phone);
+        return String.format("%s,%s", getName(), getPhone());
+    }
+}
+class Name {
+    String first;
+    String last;
+
+    public Name(String name) throws ArrayIndexOutOfBoundsException {
+        String[] names = name.split(" ");
+        this.first = names[0];
+        this.last = names[1];
+    }
+
+    public Name(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getFullName() {
+        return String.format("%s %s", first, last);
     }
 }
